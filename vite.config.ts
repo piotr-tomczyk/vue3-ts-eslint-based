25f9511949e4094efefb5623626087c3e2bb0c05
Piotr Tomczyk [Appfire] <piotr.tomczyk@appfire.com>
2023-03-04 15:19:07 +0100

    Add workflow and change vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,15 @@
 import { defineConfig } from 'vite';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import vue from '@vitejs/plugin-vue';
+import { fileURLToPath, URL } from 'url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [vue()],
+    base: './',
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url)),
+        },
+    },
 });
